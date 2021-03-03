0f3ea7d2b75654d2d5f6f7e6d9204f5ae108f719
Bestian Tang 小巴 <bestian@gmail.com>
2021-03-03 19:31:13 +0800

    deploy

---
+++ precache-manifest.a4e6e29d08dacef9f596c45d588d7000.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a96ca4001ddf0d26bf6",
+    "url": "/pytha/css/app.6155116c.css"
+  },
+  {
+    "revision": "64394db03ab637f99ee99ef6aa598d5f",
+    "url": "/pytha/img/logo.64394db0.png"
+  },
+  {
+    "revision": "eb6fa1a9e8a8931313df27ff73c808d0",
+    "url": "/pytha/index.html"
+  },
+  {
+    "revision": "2a96ca4001ddf0d26bf6",
+    "url": "/pytha/js/app.46cc7c50.js"
+  },
+  {
+    "revision": "621897d4d5ff9d769fd9",
+    "url": "/pytha/js/chunk-vendors.82f7f181.js"
+  },
+  {
+    "revision": "64394db03ab637f99ee99ef6aa598d5f",
+    "url": "/pytha/logo.png"
+  },
+  {
+    "revision": "a18d98002b3e3dd4a063e9f2225f6c6e",
+    "url": "/pytha/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pytha/robots.txt"
+  }
+]);
